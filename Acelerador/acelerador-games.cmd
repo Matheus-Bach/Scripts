d5c1584964ef14a0a9a5d3ee050b355123f4e28a
Matheus Bach <35426162+Matheus-Bach@users.noreply.github.com>
2018-07-14 12:12:46 -0300

    Acelerador funcionando basicamente por taskkill de processos pré-definidos
    Projeto apresentou bugs ao iniciar alguns tipos de jogos (não corrigido)

---
+++ Acelerador/acelerador-games.cmd
@@ -0,0 +1,87 @@
+@echo off
+echo.
+:in
+color 0a
+cls
+title.     Acelerador Ultra Gamer v 1.2 (alpha)
+cls
+mode 55,9
+echo.
+echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
+echo                 Acelerador Ultra Gamer
+echo                 (criado por MB_Player)           v 1.3 
+echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
+echo   Araste seu executável do jogo aqui e tecle "ENTER"
+echo        ex: C:/games/jogo top/seujogo.exe
+echo.
+set /p pe2= .  Arraste aqui o seu jogo ^>Ä 
+echo.
+echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
+cls
+echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
+echo                A C E L E R A N D O . . .
+echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
+timeout 2
+taskkill -f /im explorer.exe
+taskkill -f /im explorer.exe
+taskkill -f /im explorer.exe
+taskkill -f /im explorer.exe
+taskkill -f /im System
+taskkill -f /im wininit.exe
+taskkill -f /im services.exe
+taskkill -f /im lsass.exe
+taskkill -f /im svchost.exe
+taskkill -f /im svchost.exe
+taskkill -f /im svchost.exe
+taskkill -f /im svchost.exe
+taskkill -f /im svchost.exe
+taskkill -f /im svchost.exe
+taskkill -f /im svchost.exe
+taskkill -f /im igfxCUIService.exe
+taskkill -f /im svchost.exe
+taskkill -f /im AvastSvc.exe
+taskkill -f /im svchost.exe
+taskkill -f /im spoolsv.exe
+taskkill -f /im afwServ.exe
+taskkill -f /im svchost.exe
+taskkill -f /im svchost.exe
+taskkill -f /im svchost.exe
+taskkill -f /im SynTPEnhService.exe
+taskkill -f /im dasHost.exe
+taskkill -f /im svchost.exe
+taskkill -f /im PresentationFontCache.exe
+taskkill -f /im SearchIndexer.exe
+taskkill -f /im IntelCpHeciSvc.exe
+taskkill -f /im audiodg.exe
+taskkill -f /im csrss.exe
+taskkill -f /im winlogon.exe
+taskkill -f /im dwm.exe
+taskkill -f /im SynTPEnh.exe
+taskkill -f /im sihost.exe
+taskkill -f /im taskhostw.exe
+taskkill -f /im igfxEM.exe
+taskkill -f /im igfxHK.exe
+taskkill -f /im igfxTray.exe
+taskkill -f /im SynTPHelper.exe
+taskkill -f /im explorer.exe
+taskkill -f /im RuntimeBroker.exe
+taskkill -f /im ShellExperienceHost.exe
+taskkill -f /im SearchUI.exe
+taskkill -f /im ApplicationFrameHost.exe
+taskkill -f /im svchost.exe
+taskkill -f /im wmplayer.exe
+taskkill -f /im conhost.exe
+taskkill -f /im WmiPrvSE.exe
+start %pe2%
+cls
+echo.
+echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
+echo      Ganho de desempenho concluído. Para retornar
+echo       ao estado normal digite qualquer coisa aqui
+echo    .................................................
+pause
+cls
+start C:\Windows\explorer.exe
+echo Essa eh uma versão de testes e por isso apresenta varios bugs e ainda poucos recursos. Com o tempo irei melhorar o programa. Obrigado por usa-lo
+timeout 40
+goto in
