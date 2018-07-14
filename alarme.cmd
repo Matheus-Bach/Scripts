405fcce60fc6a879792500df75d37a68b6e92975
Matheus Bach <35426162+Matheus-Bach@users.noreply.github.com>
2018-07-14 12:46:04 -0300

    Alarme improvisado
    Sem celular nem outro software de despertador, fiz isso para me tirar do sono pesado em plena madrugada pra jogar aquele CS

---
+++ alarme.cmd
@@ -0,0 +1,29 @@
+goto 1
+set /p v= .       Digite  ^>Ä 
+timeout %v%
+:1
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout 500
+start "C:\Program Files\Windows Media Player\wmplayer.exe" "%windir%/123.mp3"
+timeout %v%
+exit
